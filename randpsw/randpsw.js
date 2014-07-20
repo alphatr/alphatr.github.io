85ac4981560e4b56bd628debbd2f2daa86376b5e
AlphaTr <trefoiles@gmail.com>
2014-07-20 15:26:45 +0800

    add randpsw

---
+++ randpsw/randpsw.js
@@ -0,0 +1,63 @@
+/* global BigInteger, md5 */
+(function (ns) {
+    'use strict';
+
+    /**
+     * 生成随机密码
+     * @param  {[type]} value 初始密码值
+     * @param  {[type]} uri   密码使用的网页
+     * @param  {[type]} salt  salt值
+     */
+    function randpsw(value, domain, salt) {
+        salt = salt || '';
+        value = value.toString();
+
+        var hash = new BigInteger('0x' + md5(value + domain + salt)),
+            pswArr = value.match(/./g) || [],
+            i = 0, length = pswArr.length,
+            randpswArr = new Array(length),
+
+            // 替换密码算法
+            replacePswChar = function (index, type) {
+                var charList = [
+                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                    '0123456789',
+                    '~!@#$%^&*'
+
+                ],
+                length = charList[type].length,
+                aryNum = hash.toString(length);
+
+                index = index % aryNum.length;
+                return charList[type].charAt(parseInt(aryNum.charAt(index), length));
+            },
+            single;
+
+        // 循环替换
+        for (i = 0; i < length; i++) {
+            if (pswArr[i].search(/[A-Z]/) === 0) {
+                single = replacePswChar(i, 0).toUpperCase();
+            } else if (pswArr[i].search(/[a-z]/) === 0) {
+                single = replacePswChar(i, 0).toLowerCase();
+            } else if (pswArr[i].search(/[0-9]/) === 0) {
+                single = replacePswChar(i, 1);
+            } else {
+                single = replacePswChar(i, 2);
+            }
+
+            randpswArr[i] = single;
+        }
+
+        // 打乱
+        for (i = 0; i < length; i++) {
+            randpswArr = randpswArr.sort(function (a, b) {
+                var key = parseInt(md5(a + b).substr(0, 2), 16);
+                return key % 2;
+            });
+        }
+
+        return randpswArr.join('');
+    }
+
+    ns.randpsw = randpsw;
+})(this);
